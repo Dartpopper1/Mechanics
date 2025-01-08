8636c2e5fa765f26bc8205347413105f372f39cb
Dartpopper1 <espinozaj@roverkids.org>
2025-01-08 13:28:08 -0500

    Finsihed 4.2

---
+++ Mechanics/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+    }
+}
